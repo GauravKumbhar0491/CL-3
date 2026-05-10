12bcfb92c51b353ed0f13c9a3ac728fe597093f7
Gunjan Rane <145116107+gunjanrane12@users.noreply.github.com>
2026-05-10 23:18:32 +0530

    Add files via upload

---
+++ 7/rmi_client.py
@@ -0,0 +1,12 @@
+import Pyro4
+
+uri = input("Enter Server URI: ")
+
+server = Pyro4.Proxy(uri)
+
+s1 = input("Enter first string: ")
+s2 = input("Enter second string: ")
+
+result = server.concatenate(s1, s2)
+
+print("Concatenated String:", result)
